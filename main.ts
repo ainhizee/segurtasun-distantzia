aff7b6dff223788510af242fdcb058666316b856
ainhizee <93262250+ainhizee@users.noreply.github.com>
2022-01-14 14:14:05 +0100

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,15 @@
 radio.onReceivedNumber(function (receivedNumber) {
-    if (radio.receivedPacket(RadioPacketProperty.SignalStrength) > -60) {
+    if (radio.receivedPacket(RadioPacketProperty.SignalStrength) > -20) {
         basic.showIcon(IconNames.Yes)
     } else {
         basic.showIcon(IconNames.No)
+    }
+    if (radio.receivedPacket(RadioPacketProperty.SignalStrength) < -20) {
+        basic.showIcon(IconNames.Sad)
     }
 })
 radio.setGroup(100)
 radio.setTransmitPower(3)
 basic.forever(function () {
-    radio.sendNumber(0)
+    radio.sendNumber(6)
 })
